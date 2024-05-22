7c6cf35db4559bf8823e24c12f595c250ad54dfb
Bartek-Pisklak <bartek.pisklak@onet.pl>
2024-05-23 01:17:11 +0200

    clean-architecture-test

---
+++ HeavyEquipmentRental/src/HeavyEquipmentRental.Web/Contributors/Update.UpdateContributorResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace HeavyEquipmentRental.Web.Contributors;
+
+public class UpdateContributorResponse(ContributorRecord contributor)
+{
+  public ContributorRecord Contributor { get; set; } = contributor;
+}
